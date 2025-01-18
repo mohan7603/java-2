5ca2c862b109a27a453b75abee8d2ebbd464d5b9
Mohan <146535223+mohan7603@users.noreply.github.com>
2025-01-18 04:26:56 -0800

    Add files via upload

---
+++ Letter_Count.java
@@ -0,0 +1,26 @@
+// find letter count in given sentence:
+
+import java.util.Scanner;
+
+public class Letter_Count{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("ENTER A SENTENCES : ");
+        String Sentence = scan.nextLine();
+
+        int Count = 0;
+
+        Sentence = Sentence.toLowerCase();
+
+        for(int i=0;i<Sentence.length();i++){
+            char ch = Sentence.charAt(i);
+
+            if(ch >= 'a' && ch <= 'z'){
+                Count++;
+            }
+        }
+        System.out.println("No of Sentence Count is : "+Count);
+
+    }
+}
